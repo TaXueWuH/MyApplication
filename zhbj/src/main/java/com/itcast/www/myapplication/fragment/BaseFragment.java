e1f2d589cdb676acace6132fb1123c7c24b55528
liangsilin <liang-silin@hotmail.com>
2016-06-05 00:33:42 +0800

    底部导航搭建+viewPager适配器

--- zhbj/src/main/java/com/itcast/www/myapplication/fragment/BaseFragment.java
+++ zhbj/src/main/java/com/itcast/www/myapplication/fragment/BaseFragment.java
@@ -2,7 +2,6 @@ package com.itcast.www.myapplication.fragment;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +14,9 @@ public abstract class BaseFragment  extends Fragment{
 
     public Context context;
 
-    @Nullable
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         context = getActivity();
         View view = initView();
         initData();
--- zhbj/src/main/java/com/itcast/www/myapplication/fragment/ContentFragment.java
+++ zhbj/src/main/java/com/itcast/www/myapplication/fragment/ContentFragment.java
@@ -1,27 +1,114 @@
 package com.itcast.www.myapplication.fragment;
 
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
-import android.widget.TextView;
+import android.view.ViewGroup;
+import android.widget.RadioGroup;
+
+import com.itcast.www.myapplication.R;
+import com.itcast.www.myapplication.pager.BasePager;
+import com.itcast.www.myapplication.pager.GovPage;
+import com.itcast.www.myapplication.pager.HomePage;
+import com.itcast.www.myapplication.pager.NewsCenterPage;
+import com.itcast.www.myapplication.pager.SettingPage;
+import com.itcast.www.myapplication.pager.SmartSevicePage;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by LSL on 2016/5/22.
  */
 public class ContentFragment extends BaseFragment{
 
-    private TextView textView;
+    private final class OnMyCheckedChangeListener implements RadioGroup.OnCheckedChangeListener {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int checkId) {
+
+            int item = 0;
+            switch(checkId){
+
+                case R.id.rb_home:
+                    item = 0;
+                    break;
+                case R.id.rb_news_center:
+                    item = 1;
+                    break;
+                case R.id.rb_smart_service:
+                    item = 2;
+                    break;
+                case R.id.rb_gov_affairs:
+                    item = 3;
+                    break;
+                case R.id.rb_setting:
+                    item= 4;
+                    break;
+                default:
+                    break;
+            }
+            vp_home.setCurrentItem(item);
+
+        }
+    }
+
+    @ViewInject(R.id.vp_home)
+    private ViewPager vp_home;
+    @ViewInject(R.id.rg_home)
+    private RadioGroup rg_home;
 
     @Override
     public View initView() {
 
-        textView= new TextView(context);
+        View view = View.inflate(context, R.layout.fragment_home,null);
+        ViewUtils.inject(this,view);
 
-        return textView;
+        rg_home.setOnCheckedChangeListener(new OnMyCheckedChangeListener());
+        rg_home.check(R.id.rb_home);
+        return view;
     }
+
+    private List<BasePager> pages = new ArrayList<>();
 
     @Override
     public Void initData() {
-        textView.setText("我是内容界面");
+        pages.add(new HomePage(context));
+        pages.add(new NewsCenterPage(context));
+        pages.add(new SmartSevicePage(context));
+        pages.add(new GovPage(context));
+        pages.add(new SettingPage(context));
+
+        MyPagerAdapter  adapter= new MyPagerAdapter();
+        vp_home.setAdapter(adapter);
         return null;
     }
 
+    class MyPagerAdapter extends PagerAdapter{
+        @Override
+        public int getCount() {
+            return pages.size();
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            BasePager currentPage = pages.get(position);
+            View currentView =  currentPage.initView();
+            currentPage.initData();
+            container.addView(currentView);
+            return currentView;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view==object;
+        }
+    }
 }
