b1edbc7202ff0effc37ae7fb10152babe45fc9ba
liangsilin <liang-silin@hotmail.com>
2016-06-04 18:32:30 +0800

    图片资源文件的导入和网络地址的初始化

---
+++ zhbj/src/main/java/com/itcast/www/myapplication/utils/NetUrl.java
@@ -0,0 +1,13 @@
+package com.itcast.www.myapplication.utils;
+/**
+ * 网络url
+ * @author wangdh
+ *
+ */
+public class NetUrl {
+    public final static String BASE_URL = "http://10.0.3.2:8080/zhbj";
+    //新闻中心的接口url
+    public final static String NEWS_CENTER_CATEGORIES = BASE_URL + "/categories.json";
+    public static final String PHOTOS_URL = BASE_URL + "/photos/photos_1.json";
+    
+}
