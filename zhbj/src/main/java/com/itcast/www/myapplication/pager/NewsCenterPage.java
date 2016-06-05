df2635f13f5da0397f09f573e64085d2f2665d26
liangsilin <liang-silin@hotmail.com>
2016-06-05 14:47:44 +0800

    新闻中心通过HttpUtils工具请求网络数据

--- zhbj/src/main/java/com/itcast/www/myapplication/pager/NewsCenterPage.java
+++ zhbj/src/main/java/com/itcast/www/myapplication/pager/NewsCenterPage.java
@@ -1,8 +1,16 @@
 package com.itcast.www.myapplication.pager;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+
+import com.itcast.www.myapplication.utils.NetUrl;
+import com.lidroid.xutils.HttpUtils;
+import com.lidroid.xutils.exception.HttpException;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.http.callback.RequestCallBack;
+import com.lidroid.xutils.http.client.HttpRequest;
 
 /**
  * Created by LSL on 2016/6/4.
@@ -27,5 +35,21 @@ public class NewsCenterPage extends BasePager {
     @Override
     public void initData() {
         textView.setText("我是新闻中心");
+
+        HttpUtils httpUtils = new HttpUtils();
+        httpUtils.send(HttpRequest.HttpMethod.GET,NetUrl.NEWS_CENTER_CATEGORIES,  null, new RequestCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                String result = responseInfo.result;
+
+                Log.i("NewsCenterPage",result);
+            }
+
+            @Override
+            public void onFailure(HttpException error, String msg) {
+                error.printStackTrace();
+                Log.i("NewsCenterPage","请求失败"+msg);
+            }
+        });
     }
 }
